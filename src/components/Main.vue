d7dbd48e925f474a014101cec338bfe12e91d654
刘玄 <980415069@qq.com>
2016-09-11 21:48:32 +0800

    主页面完成

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,10 +9,28 @@
 	<div>
 		<ul class="clearfix vue-main-search-ul">
 			<li class="clearfix" v-for="search in searchs">
-				<a href="" class="vue-main-search" v-bind:class="{'vue-main-search-hover':search.status}">{{ search.item }}</a>
+				<a class="vue-main-search" v-bind:class="{'vue-main-search-hover':search.status}">{{ search.item }}</a>
+			</li>
+		</ul>
+		<ul class="clearfix vue-main-body-ul">
+			<li class="clearfix" v-for="body in bodys">
+				<img v-bind:src="body.link" alt="">
+				<div style="padding-right:110px;">
+					<p>
+						<a href="" class="vue-body-li-author">{{ body.author }}</a> -
+						<span class="vue-body-li-time">大约<span v-if="body.time>24">{{ Math.floor(body.time/24) }}天</span><span v-else> {{ body.time }}小时</span>之前</span>
+					</p>
+					<p>
+						<a href="" class="vue-body-li-title">{{ body.item }}</a>
+					</p>
+					<p class="vue-body-li-foot">
+						<span v-show="body.read != ''">阅读 {{ body.read}}</span><span v-show="body.comment != ''"> - 评论 {{ body.comment }}</span><span v-show="body.like != ''"> - 喜欢 {{ body.like}}</span><span v-show="body.spare != ''"> - 打赏 {{ body.spare }}</span>
+					</p>
+				</div>
 			</li>
 		</ul>
 	</div>
+
 </template>
 
 <script>
@@ -20,7 +38,7 @@
 	export default{
 		data(){
           return {
-          	 titles:[
+          	titles:[
           	 	{item:"发现",status:true},
           	 	{item:"2015年精选"}          	
           	],
@@ -31,6 +49,98 @@
           		{item:"七日热门"},
           		{item:"三十日热门"},
           		{item:"有奖活动"},
+          	],
+          	bodys:[
+          		{item:"当哈佛凌晨四点半是个谎言，我们的努力还有什么意义？",
+          		link:require('src/assets/body01.jpg'),
+          		author:"斜杠少年白",
+          		time:"12",
+          		read:"7477",
+          		comment:"55",
+          		like:"295",
+          		spare:"2"
+          		},
+          		{item:"不成遍体鳞伤，怎么内心强大？",
+          		link:require('src/assets/body02.jpg'),
+          		author:"十两",
+          		time:"9",
+          		read:"6518",
+          		comment:"78",
+          		like:"677",
+          		spare:"",
+          		},
+          		{item:"我心里，挥之不去的灵魂",
+          		link:require('src/assets/body03.jpg'),
+          		author:"冷眼",
+          		time:"8",
+          		read:"4274",
+          		comment:"65",
+          		like:"221",
+          		spare:"1",
+          		},
+          		{item:"蝉的风",
+          		link:require('src/assets/body04.jpg'),
+          		author:"十二君子",
+          		time:"3",
+          		read:"894",
+          		comment:"12",
+          		like:"50",
+          		spare:"2",
+          		},
+          		{item:"荒诞故事——狗邻居",
+          		link:require('src/assets/body05.jpg'),
+          		author:"路人",
+          		time:"54",
+          		read:"7958",
+          		comment:"34",
+          		like:"93",
+          		spare:"",
+          		},
+          	{item:"当哈佛凌晨四点半是个谎言，我们的努力还有什么意义？",
+          		link:require('src/assets/body01.jpg'),
+          		author:"斜杠少年白",
+          		time:"12",
+          		read:"7477",
+          		comment:"55",
+          		like:"295",
+          		spare:"2"
+          		},
+          		{item:"不成遍体鳞伤，怎么内心强大？",
+          		link:require('src/assets/body02.jpg'),
+          		author:"十两",
+          		time:"9",
+          		read:"6518",
+          		comment:"78",
+          		like:"677",
+          		spare:"",
+          		},
+          		{item:"我心里，挥之不去的灵魂",
+          		link:require('src/assets/body03.jpg'),
+          		author:"冷眼",
+          		time:"8",
+          		read:"4274",
+          		comment:"65",
+          		like:"221",
+          		spare:"1",
+          		},
+          		{item:"蝉的风",
+          		link:require('src/assets/body04.jpg'),
+          		author:"十二君子",
+          		time:"3",
+          		read:"894",
+          		comment:"12",
+          		like:"50",
+          		spare:"2",
+          		},
+          		{item:"荒诞故事——狗邻居",
+          		link:require('src/assets/body05.jpg'),
+          		author:"路人",
+          		time:"54",
+          		read:"7958",
+          		comment:"34",
+          		like:"93",
+          		spare:"",
+          		},
           	],
           	mainWidth: (document.body.clientWidth - 170)*0.75 <= 1000 ? (document.body.clientWidth - 170)*0.75:1000
           }
@@ -56,6 +166,7 @@
 		border-bottom: 2px solid #d9d9d9;
 		padding-left: 20px;
 		position:fixed;
+		background: #fff;
 	}
 	.vue-main-a{
 		float: left;
@@ -84,6 +195,44 @@
 				color:#fff;
 				border-color: #e78170;
 			}
+		}
+	}
+	.vue-main-body-ul{
+		margin-top: 20px;
+		padding:15px 0 0 20px;
+		& li{
+			padding-bottom: 15px;
+			margin-bottom: 15px;
+			border-bottom:1px dashed #d9d9d9;
+			& p{
+				margin:8px 0;
+			}
+			& a.vue-body-li-author{
+				color:#4094c7;
+				&:hover{
+					color:#075b8d;
+				}
+			}
+			& span.vue-body-li-time{
+				color:#717171;
+			}
+			& a.vue-body-li-title{
+				font-size: 18px;
+				font-weight: 600;
+				line-height: 1.5;
+			}
+			& img{
+				float: right;
+				width: 100px;
+				height: 100px;
+				border-radius: 4px;
+			}
+			& p.vue-body-li-foot span{
+				color:#999999;
+			}
+		}
+		& li:last-child{
+			border:none;
 		}
 	}
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="vue-navbar-top">
-		<a href="" class="vnt-a" v-bind:class="{'vnt-a-hover': item.title === '首页' }" v-for="item in items">{{ item.title }}</a>
+		<a href="" @mouseleave="evtSideLeave" @mouseenter="evtSideEnter($index,item.title)" class="vnt-a" v-bind:class="{'vnt-a-hover': item.title === '首页' }" v-for="item in items">{{ item.title }}</a>
+		<div class="vue-navbar-right" :style="{top: Top + 'px'}" v-show="Status" transition="expand">
+			<div class="tooltip"></div>
+			<div class="inner">{{ Title }}</div>
+		</div>
 	</div>
 	<div class="vue-navbar-down">
 		&nbsp;
@@ -10,26 +14,36 @@
 	export default{
 		data(){
 			return {
+				Status:false,
+				Top:'',
+				Title:'',
 				items:[
 				{title:'首页'},
 				{title:'专题'},
 				{title:'Demo'},
 				{title:'正在施工...'},
 				],
-				
 			}
 		},
 		methods: {
-           
+           evtSideLeave:function () {
+           	 this.Status = false;
+           },
+           evtSideEnter:function (e,title) {
+             this.Top = e*45+8;
+             this.Title = title;
+           	 this.Status = true;
+           }
         }
 
 	}
 </script>
-<style>
-.vue-navbar-w{
+<style lang='less'>
+ .vue-navbar-w{
 	width: 170px;
 	background: #2a2a2a;
 	position: fixed;
+	z-index: 1000;
 }
 .vnt-a{
 	padding: 10px 15px;
@@ -51,5 +65,42 @@
 .vnt-a:hover{
 	background: #f49484;
 	color:#fff;
+}
+.vue-navbar-right{
+	position: absolute;
+	left: 175px;
+	opacity: 0.8;
+	padding-left: 5px;
+	& .tooltip{
+		top:50%;
+		left: 0;
+		margin-top: -5px;
+		border-width: 5px 5px 5px 0;
+		border-right-color: #000;
+		position: absolute;;
+		width: 0;
+		height: 0;
+		border-left-color: transparent;
+		border-top-color: transparent;
+		border-bottom-color: transparent;
+		border-style: solid;	
+	}
+	& .inner{
+		color:#fff;
+		padding: 8px;
+		text-align: center;
+		border-radius: 4px;
+		white-space:nowrap;
+		background-color: #000;
+	}
+}
+
+.expand-transition{
+	transition:all .3s ease;
+	left: 175px;
+}
+.expand-enter, .expand-leave{
+    opacity: 0;
+    left: 170px;
 }
 </style>
